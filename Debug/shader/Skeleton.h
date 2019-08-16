5ae2c07807636790c1a7263193250602cbcf188a
wzaqrh <652440522@qq.com>
2019-08-17 03:23:48 +0800

    Skinning去掉循环

--- Debug/shader/Skeleton.h
+++ Debug/shader/Skeleton.h
@@ -11,18 +11,10 @@ cbuffer cbWeightedSkin : register(b1)
 
 float4 Skinning(float4 iBlendWeights, int4 iBlendIndices, float4 iPos)
 {
-    float BlendWeights[4] = (float[4])iBlendWeights;
-	BlendWeights[3] = 1.0 - BlendWeights[0] - BlendWeights[1] - BlendWeights[2];
-    int BlendIndices[4] = (int[4])iBlendIndices;	
-	
-    float4	Pos = float4(0.0,0.0,0.0,iPos.w);   
-	const int NumBones = 4;
-    for (int iBone = 0; iBone < NumBones; iBone++) {
-		int Indice = BlendIndices[iBone];
-		float Weight = BlendWeights[iBone];
-			
-		float4 bonePos = mul(iPos, Models[Indice]);
-        Pos.xyz += bonePos.xyz * Weight;
-    }
+    float4 Pos = float4(0.0,0.0,0.0,iPos.w); 	
+	Pos.xyz += mul(iPos, Models[iBlendIndices.x]).xyz * iBlendWeights.x;
+	Pos.xyz += mul(iPos, Models[iBlendIndices.y]).xyz * iBlendWeights.y;
+	Pos.xyz += mul(iPos, Models[iBlendIndices.z]).xyz * iBlendWeights.z;
+	Pos.xyz += mul(iPos, Models[iBlendIndices.w]).xyz * (1.0 - iBlendWeights.x - iBlendWeights.y - iBlendWeights.z);
 	return Pos;
 }
