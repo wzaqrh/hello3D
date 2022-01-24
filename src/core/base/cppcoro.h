54c7747359f735a38a6d5a19be9b1f00d5997344
wzaqrh <652440522@qq.com>
2022-01-25 00:52:42 +0800

    添加第三方库cppcoro

---
+++ src/core/base/cppcoro.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <cppcoro/task.hpp>
+#include <cppcoro/shared_task.hpp>
+#include <cppcoro/static_thread_pool.hpp>
+#include <cppcoro/io_service.hpp>
+#include <cppcoro/sync_wait.hpp>
