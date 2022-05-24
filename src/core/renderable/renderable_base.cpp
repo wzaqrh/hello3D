b3e4cc64a26cdfca5cd24ea935a760207781daeb
wzaqrh <652440522@qq.com>
2022-05-24 23:41:08 +0800

    fix bug RenderableSingleRenderOp.GetWorldAABB可能返回nan aabb

--- src/core/renderable/renderable_base.cpp
+++ src/core/renderable/renderable_base.cpp
@@ -19,9 +19,14 @@ const ITexturePtr& RenderableSingleRenderOp::GetTexture() const
 
 Eigen::AlignedBox3f RenderableSingleRenderOp::GetWorldAABB() const
 {
-	Transform3fAffine t(GetTransform()->GetWorldMatrix());
-	Eigen::AlignedBox3f worldAABB = mAABB.transformed(t);
-	return worldAABB;
+	Eigen::AlignedBox3f bounds = mAABB;
+	if (!mAABB.isEmpty()) {
+		if (auto transform = this->GetTransform()) {
+			Transform3fAffine t(transform->GetWorldMatrix());
+			bounds = mAABB.transformed(t);
+		}
+	}
+	return bounds;
 }
 
 void RenderableSingleRenderOp::SetTexture(const ITexturePtr& texture)
